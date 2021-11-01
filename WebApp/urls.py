04938dbac5b32d349ad0681a6dfd669e2a2869ac
Eduardo Lillo <eduardo.lillo@usm.cl>
2021-10-31 23:23:44 -0300

    implementacion de registro/login demo'

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import quimica, index, matematica, iniciar_sesion,registrarse
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', iniciar_sesion, name="iniciar_sesion"),
+    path('home/',index,name='home'),
     path('matematica/', matematica, name="matematica"),
     path('quimica/', quimica, name="quimica"),
-    path('iniciar_sesion/', iniciar_sesion, name="iniciar_sesion"),
+    #path('iniciar_sesion/', iniciar_sesion, name="iniciar_sesion"),
     path('registrarse/', registrarse, name="registrarse"),
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.db.models.manager import EmptyManager
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
@@ -12,7 +14,28 @@ def quimica(request):
     return render(request,'app/quimica.html')
 
 def iniciar_sesion(request):
+    if request.method == 'POST':
+        correo = request.POST['correo']
+        contrasena = request.POST['contraseña']
+        user = authenticate(username=correo, password=contrasena)
+        print(user)
+        if user is not None:
+            return redirect('home')
     return render(request,'app/iniciar_sesion.html')
 
 def registrarse(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        correo = request.POST['correo']
+        contraseña = request.POST['contraseña']
+        user = User.objects.create_user(username=correo,password=contraseña,email=correo,last_name=apellido,first_name=nombre,)
+        user.save()
+        return redirect('home')
     return render(request,'app/registrarse.html')
+
+def foro(request):
+    pass
+
+def perfil_user(request):
+    pass

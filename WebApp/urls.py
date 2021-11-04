1f7e308f5666b1b7d5465bb8831f618bf562ea42
Eduardo Lillo <eduardo.lillo@usm.cl>
2021-11-03 21:33:05 -0300

    Sistema para designar cuantas preguntas por tema

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import quimica, index, matematica, iniciar_sesion,registrarse,logout_view
+from .views import quimica, index, matematica, iniciar_sesion,registrarse,logout_view,certamen
 
 urlpatterns = [
     path('', iniciar_sesion, name="iniciar_sesion"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('quimica/', quimica, name="quimica"),
     path('logout/', logout_view, name="logout"),
     path('registrarse/', registrarse, name="registrarse"),
+    path('certamen/',certamen,name='certamen')
 ]

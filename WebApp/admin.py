f72c34cafbaf089cd350983113f6031d0c84599b
Nicolas_A_C <nicolas.armijoc@sansano.usm.cl>
2021-11-09 20:48:38 -0300

    estructura basica del foro y comentarios

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-
-from WebApp.models import PreguntasMate, preguntaPrueba
+from WebApp.models import PreguntasMate, preguntaPrueba, PostForo, Comentario
 
 class PreguntasMateAdmin(admin.ModelAdmin):
     list_display = ("siglas","dificultad","tema")
     search_fields = ("siglas","contenido")
     list_filter = ("siglas", "dificultad", "tema",)
 
+class PostForoAdmin(admin.ModelAdmin):
+    search_fields = ('usuario','sigla','tiempo')
+    list_filter = ('sigla','tiempo',)
+    list_display = ('usuario','sigla','tiempo')
+
 admin.site.register(PreguntasMate,PreguntasMateAdmin)
-
-
+admin.site.register(PostForo, PostForoAdmin)
+admin.site.register(Comentario)
 #prueba de preguntas
 admin.site.register(preguntaPrueba)
--- WebApp/urls.py
+++ WebApp/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls import url
 from django.urls import path
-from .views import crearPregunta, index, matematica, iniciar_sesion,registrarse,logout_view, foro1, mi_perfil, certamen, guardarPregunta, certamen
+from django.urls.conf import re_path
+from .views import crearPregunta, index, matematica, iniciar_sesion,registrarse,logout_view, foro, mi_perfil, certamen, guardarPregunta, certamen, Comentarios_pk, comentario_id
 
 urlpatterns = [
     path('', iniciar_sesion, name="iniciar_sesion"),
@@ -8,10 +10,11 @@ urlpatterns = [
     path('crearPregunta/', crearPregunta, name="crearPregunta"),
     path('logout/', logout_view, name="logout"),
     path('registrarse/', registrarse, name="registrarse"),
-    path('foro/', foro1, name="foro1"),
+    path('foro/', foro, name="foro1"),
     path('mi_perfil/', mi_perfil, name="mi_perfil"),
     path('certamen/', certamen, name="certamen"),
-    #prueba
+    path('post_id/<pk>/', Comentarios_pk, name='foro2'),
+    path('comentario_id/<pk>/', comentario_id, name='foro3'),
     path('guardarPregunta/', guardarPregunta),
     path('certame/',certamen)
 ]

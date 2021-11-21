1890da62112d2414f2ca07f45451cba794d3ae3c
Ben Camus <benjamincamus9@gmail.com>
2021-11-21 04:49:45 -0300

    bugs

--- WebApp/static/app/js/MAT021.js
+++ WebApp/static/app/js/MAT021.js
@@ -1,9 +1,15 @@
 function showtime() {
+    var tiempo = document.getElementById("timeSelec");
+
     var element = document.getElementById("time");
     var check = document.getElementById("cboxTime");
     if (check.checked) {
         element.style.display = "block";
+        tiempo.setAttribute("value", "00:00");
     } else {
         element.style.display = "none";
+        tiempo.setAttribute("value", "");
     }
 }
+
+console.log("zsdfsa");
